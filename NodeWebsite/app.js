95853f0e63e28365474568222c8fe81fd35240dd
Livio <103258107+liviodorig@users.noreply.github.com>
2023-12-20 14:32:01 +0100

    Added package and code to app.ja

--- NodeWebsite/app.js
+++ NodeWebsite/app.js
@@ -8,7 +8,67 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-
 app.listen(port, () => {
   console.log(`Server läuft auf http://localhost:${port}`);
 });
+
+
+const express = require('express');
+const multer = require('multer');
+const AWS = require('aws-sdk');
+const fs = require('fs');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+// Konfiguriere Multer (Hier kannst du auch das Speicherverhalten anpassen)
+const upload = multer({ dest: 'uploads/' });
+
+// AWS SDK konfigurieren
+AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: 'deine-region'
+});
+const s3 = new AWS.S3();
+
+app.post('/upload', upload.single('image'), function (req, res) {
+    const file = req.file;
+    const scale = req.body.scale;
+
+    // Hier kannst du eventuell das Bild basierend auf dem Skalierungsfaktor bearbeiten
+
+    // Lies die Datei
+    const fileContent = fs.readFileSync(file.path);
+
+    // Konfiguriere die Upload-Parameter
+    const params = {
+        Bucket: 'dein-bucket-name', // Ersetze durch deinen Bucket-Namen
+        Key: file.originalname, // Dateiname im Bucket
+        Body: fileContent
+    };
+
+    // Lade das Bild zu S3 hoch
+    s3.upload(params, function (err, data) {
+        if (err) {
+            throw err;
+        }
+        console.log(`File uploaded successfully. ${data.Location}`);
+
+        // Antwort an den Client senden
+        res.json({ message: 'Upload erfolgreich!', location: data.Location });
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server läuft auf http://localhost:${port}`);
+});
+
+
+
+
+//start server: node app.js
+
+//server stoppen: ctrl + c
--- NodeWebsite/public/main.js
+++ NodeWebsite/public/main.js
@@ -3,76 +3,46 @@ document.addEventListener("DOMContentLoaded", function () {
    var submitButton = document.getElementById("submit-button");
    var previewContainer = document.getElementById("image-preview");
 
-   var AWS = require('aws-sdk');
-   var s3 = new AWS.S3();
-
    fileUpload.addEventListener("change", function (event) {
-      var file = fileUpload.files[0];
+       var file = fileUpload.files[0];
 
-      if (file && file.type === "image/png") {
-         var reader = new FileReader();
-         reader.onload = function (e) {
-            previewContainer.innerHTML =
-               '<img src="' +
-               e.target.result +
-               '" alt="Vorschau" class="image-preview">';
-            var image = new Image();
-            image.onload = function () {
-               var imageInfo = document.getElementById("image-info");
-               imageInfo.textContent = this.width + " x " + this.height;
-            };
-            image.src = e.target.result;
-         };
-         reader.readAsDataURL(file);
-      } else if (file && file.type !== "image/png") {
-         alert("Bitte wählen Sie nur eine .png Datei aus.");
-         fileUpload.value = "";
-      }
+       if (file && file.type === "image/png") {
+           var reader = new FileReader();
+           reader.onload = function (e) {
+               previewContainer.innerHTML =
+                   '<img src="' +
+                   e.target.result +
+                   '" alt="Vorschau" class="image-preview">';
+           };
+           reader.readAsDataURL(file);
+       } else if (file && file.type !== "image/png") {
+           alert("Bitte wählen Sie nur eine .png Datei aus.");
+           fileUpload.value = "";
+       }
    });
 
    submitButton.addEventListener("click", function (event) {
-      if (fileUpload.files.length === 0) {
-         alert("Bitte wählen Sie zuerst eine Datei aus!");
-         event.preventDefault();
-      } else {
-         var file = fileUpload.files[0];
-         var scaleValue = document.getElementById('scale-select').value;
-         var scaleNumber = scaleValue * 100;
-         var originalFileName = file.name;
- 
-         // Generierung des neuen Dateinamens
-         var newFileName = scaleNumber + "_" + originalFileName;
-         console.log(newFileName);
+       event.preventDefault();
+       if (fileUpload.files.length === 0) {
+           alert("Bitte wählen Sie zuerst eine Datei aus!");
+       } else {
+           var formData = new FormData();
+           var file = fileUpload.files[0];
+           formData.append("image", file);
+           formData.append("scale", document.getElementById('scale-select').value);
 
-         var newFileName = scaleNumber + "_" + originalFileName;
-         console.log(newFileName);
-         var uploadParams = {
-            Bucket: 'Ihr-Bucket-Name', // Ersetzen Sie dies durch Ihren Bucket-Namen
-            Key: fileName + ".png",
-            Body: file
-         };
-
-         function downloadImage(imageUrl) {
-            // Erstellen eines temporären 'a'-Tags
-            var downloadLink = document.createElement("a");
-            downloadLink.href = imageUrl;
-            downloadLink.download = "DownloadedImage.png";
-
-            // Hinzufügen des Links zum Dokument und Auslösen des Klicks
-            document.body.appendChild(downloadLink);
-            downloadLink.click();
-
-            // Entfernen des Links nach dem Download
-            document.body.removeChild(downloadLink);
-         }
-
-         s3.putObject(uploadParams, function(err, data) {
-            if (err) {
-              console.log(err);
-            } else {
-              console.log('Successfully uploaded image to S3');
-            }
-         });
-      }
+           fetch('/upload', {
+               method: 'POST',
+               body: formData
+           })
+           .then(response => response.json())
+           .then(data => {
+               console.log(data);
+               // Hier könnten Sie beispielsweise eine Nachricht anzeigen, dass das Bild erfolgreich hochgeladen wurde.
+           })
+           .catch(error => {
+               console.log('Fehler beim Hochladen:', error);
+           });
+       }
    });
 });
