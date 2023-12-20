3d77a231694eab6f634f813e5b2969d2c705872c
Livio <103258107+liviodorig@users.noreply.github.com>
2023-12-20 15:10:54 +0100

    Install multer

--- NodeWebsite/app.js
+++ NodeWebsite/app.js
@@ -13,29 +13,29 @@ const s3 = new AWS.S3();
 app.use(express.static('public'));
 
 app.post('/upload', upload.single('image'), function (req, res) {
-    const file = req.file;
-    const fileContent = fs.readFileSync(file.path);
+  const file = req.file;
+  const fileContent = fs.readFileSync(file.path);
 
-    const uploadParams = {
-        Bucket: 'DEIN_S3_BUCKET_NAME',
-        Key: path.basename(file.originalname),
-        Body: fileContent
-    };
+  const uploadParams = {
+    Bucket: 'DEIN_S3_BUCKET_NAME',
+    Key: path.basename(file.originalname),
+    Body: fileContent
+  };
 
-    s3.upload(uploadParams, function(err, data) {
-        if (err) {
-            console.error("Fehler beim Hochladen: ", err);
-            return res.status(500).send(err);
-        }
-        console.log(`Datei erfolgreich hochgeladen. ${data.Location}`);
-        
-        fs.unlinkSync(file.path); // Temporäre Datei löschen
+  s3.upload(uploadParams, function (err, data) {
+    if (err) {
+      console.error("Fehler beim Hochladen: ", err);
+      return res.status(500).send(err);
+    }
+    console.log(`Datei erfolgreich hochgeladen. ${data.Location}`);
 
-        res.json({ message: 'Upload erfolgreich!', url: data.Location });
-    });
+    fs.unlinkSync(file.path); // Temporäre Datei löschen
+
+    res.json({ message: 'Upload erfolgreich!', url: data.Location });
+  });
 });
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`Server läuft auf http://localhost:${port}`);
+  console.log(`Server läuft auf http://localhost:${port}`);
 });
