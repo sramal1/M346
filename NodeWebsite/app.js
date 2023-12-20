b6e4980b24e00c7a29669ef4a13fb6be47d5f477
Livio <103258107+liviodorig@users.noreply.github.com>
2023-12-20 14:49:07 +0100

    Delete comments

--- NodeWebsite/app.js
+++ NodeWebsite/app.js
@@ -7,31 +7,27 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Konfiguriere den statischen Datei-Server
 app.use(express.static('public'));
 
-// Konfiguriere Multer für das Datei-Handling
 const upload = multer({ dest: 'uploads/' });
 
 // AWS SDK konfigurieren
 AWS.config.update({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: 'deine-region' // Setze deine AWS-Region
+    region: 'deine-region'
 });
 const s3 = new AWS.S3();
 
-// Route für das Datei-Upload
 app.post('/upload', upload.single('image'), function (req, res) {
     const file = req.file;
     
-    // Lese den Inhalt der Datei
     const fileContent = fs.readFileSync(file.path);
 
     // Konfiguriere die Upload-Parameter für S3
     const params = {
-        Bucket: 'dein-bucket-name', // Ersetze dies mit deinem S3-Bucket-Namen
-        Key: path.basename(file.originalname), // Der Dateiname im S3-Bucket
+        Bucket: 'dein-bucket-name',
+        Key: path.basename(file.originalname),
         Body: fileContent,
         ContentType: file.mimetype
     };
@@ -44,7 +40,6 @@ app.post('/upload', upload.single('image'), function (req, res) {
         }
         console.log(`Datei erfolgreich hochgeladen. ${data.Location}`);
 
-        // Entferne die temporäre Datei
         fs.unlinkSync(file.path);
 
         // Sende die Antwort zurück an den Client
