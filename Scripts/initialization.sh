38688ba92daeef4165dc2963d505da933d687fb4
sramal1 <leotrim.ramadani@edu.gbssg.ch>
2023-12-21 17:34:17 +0100

    update init

--- Scripts/initialization.sh
+++ Scripts/initialization.sh
@@ -19,18 +19,30 @@ aws s3 rb s3://original-image-bucket-m346 --force
 aws s3 mb s3://$bucketName1
 aws s3 mb s3://$bucketName2
 
+aws s3api put-public-access-block \
+--bucket $bucketName1 \
+--public-access-block-configuration "BlockPublicPolicy=false"
+
+aws s3api put-bucket-ownership-controls \
+--bucket $bucketName1 \
+--ownership-controls="Rules=[{ObjectOwnership=BucketOwnerPreferred}]"
+
 cd ../image-resizer-lambda
 
 dotnet lambda delete-function $functionName
 
 dotnet lambda deploy-function --function-role LabRole $functionName
 
-aws lambda add-permission --function-name $functionName --action "lambda:InvokeFunction" --principal s3.amazonaws.com --source-arn arn:aws:s3:::$buckerName1 --statement-id $bucketName1
+aws lambda add-permission --function-name $functionName --profile default \
+ --statement-id AllowToBeInvoked \
+ --action "lambda:InvokeFunction" \
+ --principal s3.amazonaws.com \
+ --source-arn "arn:aws:s3:::original-image-bucket-m346" \
+ --source-account $ARN
 
 aws s3api put-bucket-notification-configuration --bucket $bucketName1 --notification-configuration '{"LambdaFunctionConfigurations": [{"LambdaFunctionArn": "arn:aws:lambda:us-east-1:'$ARN':function:'$functionName'","Events": ["s3:ObjectCreated:Put"]}]}'
+
 #cd ../NodeWebsite
-
-
 
 #node app.js
 
